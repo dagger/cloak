f8a4071d20daffbfe6db5b7bac37b1aa54ea74b5
Erik Sipsma <erik@sipsma.dev>
2022-08-23 19:36:39 -0700

    Enable only generating client stubs for workflows.
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- examples/todoapp/app/workflows/deploy/main.go
+++ examples/todoapp/app/workflows/deploy/main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 
 	"github.com/dagger/cloak/engine"
+	"github.com/dagger/cloak/examples/todoapp/app/workflows/deploy/gen/netlify"
 )
 
 func main() {
 	if err := engine.Start(context.Background(), &engine.Config{}, func(ctx context.Context) error {
 		fmt.Println("Hello from engine")
+		netlify.Deploy
 		return nil
 	}); err != nil {
 		panic(err)
