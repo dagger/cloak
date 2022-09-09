2099e2b8053e46bf8e1c0557036db5f64c5fcba7
Tanguy ⧓ Herrmann <tanguy@dagger.io>
2022-09-09 16:49:37 +0200

    feat: use hugo quickstart as a test
    Signed-off-by: Tanguy ⧓ Herrmann <tanguy@dagger.io>

--- examples/hugo/main.go
+++ examples/hugo/main.go
@@ -29,22 +29,30 @@ func main() {
 		}
 
 		curled, err := core.Exec(ctx, alp.Alpine.Build.ID, core.ExecInput{
-			Args: []string{"sh", "-c", "curl -L https://github.com/gohugoio/hugo/releases/download/v0.59.1/hugo_0.59.1_Linux-64bit.tar.gz | tar -xz"},
+			Args: []string{"sh", "-c", "curl -L https://github.com/gohugoio/hugo/releases/download/v0.102.3/hugo_0.102.3_Linux-64bit.tar.gz | tar -xz"},
 		})
 		if err != nil {
-			return fmt.Errorf("in curl: %w", err)
+			return fmt.Errorf("install hugo: %w", err)
 		}
 		_ = curled
 
+		themed, err := core.Exec(ctx, curled.Core.Filesystem.Exec.Fs.ID, core.ExecInput{
+			Args: []string{"sh", "-c", "git init . && git submodule init && git submodule add https://github.com/theNewDynamic/gohugo-theme-ananke.git ./mnt/themes/ananke"},
+		})
+		if err != nil {
+			return fmt.Errorf("git submodule add: %w", err)
+		}
+		_ = themed
+
 		wd, err := core.Workdir(ctx)
 		if err != nil {
-			return err
+			return fmt.Errorf("workdir: %w", err)
 		}
 
 		wdID := wd.Host.Workdir.Read.ID
 		_ = wdID
 
-		generated, err := core.ExecGetMount(ctx, curled.Core.Filesystem.Exec.Fs.ID, core.ExecInput{
+		generated, err := core.ExecGetMount(ctx, themed.Core.Filesystem.Exec.Fs.ID, core.ExecInput{
 			Mounts: []core.MountInput{
 				{
 					Fs:   wdID,
@@ -52,17 +60,17 @@ func main() {
 				},
 			},
 			Workdir: "/mnt/test",
-			Args:    []string{"sh", "-c", "/hugo --buildFuture && find /mnt/test"},
+			Args:    []string{"sh", "-c", "/hugo --buildFuture"},
 		}, "/mnt")
 		if err != nil {
-			return fmt.Errorf("in final: %w", err)
+			return fmt.Errorf("hugo generate: %w", err)
 		}
 
 		_ = generated
 
 		_, err = core.WriteWorkdir(ctx, generated.Core.Filesystem.Exec.Mount.ID)
 		if err != nil {
-			return fmt.Errorf("when writing on host: %w", err)
+			return fmt.Errorf("write on host: %w", err)
 		}
 		return nil
 	})
---
+++ examples/hugo/test/content/posts/my-first-post.md
@@ -0,0 +1,8 @@
+---
+title: "My First Post"
+date: 2022-09-09T13:38:31Z
+draft: true
+---
+# Sweet Home Alabama
+
+yes
