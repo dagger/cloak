771758eb3e55e17ba638363d7088e38717bda268
Tanguy ⧓ Herrmann <tanguy@dagger.io>
2022-09-14 15:00:27 +0200

    test: add test example for Schema()
    Signed-off-by: Tanguy ⧓ Herrmann <tanguy@dagger.io>

--- examples/go_code_first/types_test.go
+++ examples/go_code_first/types_test.go
@@ -26,4 +26,33 @@ normal:
 pointer:
 	%v`, s, ps)
 	}
+
+	//t.Log(s)
+}
+
+type T struct{}
+
+func (t T) Func1(ctx context.Context, a, b int) (string, error) {
+	return "", nil
+}
+
+func (t T) MustFunc1(a, b int) string {
+	return ""
+}
+
+func ExampleSchema() {
+	t := T{}
+
+	s, err := Schema(t)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println(s)
+	// Output:
+	// t {
+	// 		func1(i: int, i: int): string
+	// 		mustfunc1(i: int, i: int): string
+	// }
 }
